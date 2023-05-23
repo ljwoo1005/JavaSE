fd97e7288bceffa040247887e0f0bd39f1054ccb
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-05-23 20:15:20 +0900

    javaSE star

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -80,9 +80,9 @@ import java.util.Scanner;
   ***				  
  *****				
 *******		
- *****			 
-  ***			 
-   *			
+ *****			 i : 4 j : 6			j + i >= 10
+  ***			 i : 5 j : 5, 6			j + i >= 10
+   *			 i : 6 j : 4, 5, 6		j + i >= 10
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -116,6 +116,39 @@ import java.util.Scanner;
 				}
 				System.out.println();
 			}
+	}
+	
+	
+	강사님 풀이법
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		for(int i = 0; i < num; i++) {
+			for(int j = 0; j < num; j++) {
+				if(i <= num/2) {
+					if(i + j < num/2) {
+						System.out.print(' ');
+					} else if(j - i > num/2) {
+						System.out.print(' ');
+					} else {
+						System.out.print('*');
+					}
+				} else {
+					if(i - j > num/2) {
+						System.out.print(' ');
+					} else if(j + i >= num + num/2) {
+						System.out.print(' ');
+					} else {
+						System.out.print('*');
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 
 5번
@@ -170,44 +203,41 @@ import java.util.Scanner;
   
   ....			9 x 9 = 81
   
+  강사님 풀이
+  
+  public static void main(String[] args) {
+		for(int x = 0; x < 3; x++) {
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 3; j++) {
+					int dan = j + 2 + x*3;
+					if(dan < 10) {
+						System.out.printf("%d x %d = %d ", dan, i+1, dan * (i+1));
+					}
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+  
 
  
  */
 
 public class LoopHomeWork {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int num01 = scanner.nextInt();
-		
-			for(int a1=0; a1<num01-1; a1++) {
-				System.out.print(' ');
-			}
-			System.out.println('☆');
-			
-			for(int a=0; a<num01; a++) {
-				for(int i=num01-a-1; i>0; i--) {
-					System.out.print(' ');
-				}
-				for(int j=1; j<=a+1; j++) {
-					System.out.print('*');
-				}
-				for(int k=2; k<=a+1; k++) {
-					System.out.print('*');
+		for(int x = 0; x < 3; x++) {
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 3; j++) {
+					int dan = j + 2 + x*3;
+					if(dan < 10) {
+						System.out.printf("%d x %d = %d ", dan, i+1, dan * (i+1));
+					}
 				}
 				System.out.println();
 			}
-			for(int a2=0; a2<num01-2; a2++) {
-				System.out.print(' ');
-			}
-			System.out.println("| | Merry Christmas~*");
+			System.out.println();
 		}
+		
+	}
 }
-/*
-   ☆
-   *
-  ***
- *****
-******* 
-  | | Merry Christmas~*
-*/
