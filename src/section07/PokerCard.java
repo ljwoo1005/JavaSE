3712515c89fa2979c6dbcc5b4f22ad429597ea19
LJWOO <ljwoo1005@naver.com>
2023-05-30 15:37:26 +0900

    javaSE OOP

--- src/section07/PokerCard.java
+++ src/section07/PokerCard.java
@@ -1,9 +1,9 @@
 package section07;
 
 public class PokerCard {
-	public static int width = 62;
+	public static int width = 62; // static 영역에 저장되어있는 데이터의 주소값을 참조한다.
 	public static int height = 88;
 	
-	public String type = "Spade";
+	public String type = "Spade"; // 객체에 저장되어있는 데이터
 	public String number = "A";
 }
--- src/section07/Var.java
+++ src/section07/Var.java
@@ -27,19 +27,28 @@ package section07;
  * 
  * 3. 정적(static) 변수
  * 	static이 붙은 멤버변수
- * 	같은 타입의 모든 객체가 공유하는 변수
- * 	클래스명으로 접근 가능
- * 	객체 생성 없이 불러올 수 있다.
+ * 	같은 타입의 모든 객체가 공유하는 변수. 예) 카드의 길이, 높이
+ * 	클래스명으로 접근 가능. (객체명으로도 접근은 가능하지만 클래스명으로 접근하는 것이 알아보기 쉽다.)
+ * 	객체 생성 없이 불러올 수 있다. 프로그램 실행 직전 static 영역 메모리에 데이터가 올라와있기 때문.
  */
 public class Var {
 	int globalNum = 100;
 	static int globalNum2; // 변수선언하고 초기화 하지 않음
 	
 	public static void main(String[] args) {
-		int areaNum2; // 변수선언하고 초기화 하지 않음
+		int areaNum2; // 변수선언하고 초기화 하지 않음(지역변수 : 메소드가 끝날 때 같이 사라진다.)
 		
-		System.out.println("globalNum2 : " + globalNum2);
-//		System.out.println("areaNum2 : " + areaNum2);
+//		System.out.println("globalNum : " + globalNum); -> static 영역 안에서 Var 클래스의 객체가 메모리에 존재하지 않아서 오류가 발생한다.
+
+		Var var1 = new Var(); // Var 클래스의 객체 생성
+		System.out.println("globalNum : " + var1.globalNum); // 이제 static 영역에 Var 클래스의 객체가 생성됐으니 정상적으로 코드가 실행된다.
+		
+		var1.printNum(); // Var 클래스의 메소드를 실행
+		
+		getAreaNum(); // static 영역에 올라온 메소드이기 때문에 객체명을 붙이지 않아도 실행 가능하다.
+		
+		System.out.println("globalNum2 : " + globalNum2); // -> static 변수를 불러왔기 때문에 객체 생성 없이도 변수를 불러올 수 있다. 그리고 전역변수이기 때문에 초기화하지 않아도 default값이 존재한다.
+//		System.out.println("areaNum2 : " + areaNum2); -> 지역변수를 초기화하지 않아 오류가 발생한다.
 		
 		
 	}
@@ -49,7 +58,7 @@ public class Var {
 	}
 	
 	public void printNum() {
-		System.out.println("globalNum : " + globalNum);
-//		System.out.println("areaNum : " + areaNum);
+		System.out.println("globalNum : " + globalNum); // 전역변수 불러오기
+//		System.out.println("areaNum : " + areaNum); //
 	}
 }
