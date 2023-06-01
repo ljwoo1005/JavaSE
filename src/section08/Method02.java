1b50ba3c46095f398a4be622b5dd7a1da12d22e0
LJWOO <ljwoo1005@naver.com>
2023-06-01 15:05:07 +0900

    javaSE Method

--- src/section08/Method02.java
+++ src/section08/Method02.java
@@ -9,6 +9,6 @@ public class Method02 {
 	
 	// 2. 인자o 리턴x -> 인자값을 받아 실행하고 끝나는 메소드
 	public static void printName(String name) { // String name = 피카츄
-		System.out.println();
+		System.out.println(name);
 	}
 }
--- src/section08/Method06.java
+++ src/section08/Method06.java
@@ -4,7 +4,7 @@ public class Method06 {
 	public static void main(String[] args) {
 		String name = getName();
 		System.out.println("name : " + name);
-		System.out.println("name : " + getName());
+		System.out.println("name : " + getName()); // static 메소드이기 때문에 직접 호출이 가능하다.
 	}
 	
 	// 6. 인자 x 리턴 o -> 메소드를 실행하여 값을 반환한다.
