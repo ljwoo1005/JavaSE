96a8dfa96093854a5b5bf4df13504ec193fda260
LJWOO <ljwoo1005@naver.com>
2023-06-20 21:50:46 +0900

    javaSE Exception

---
+++ src/section14/ExceptionClass04.java
@@ -0,0 +1,24 @@
+package section14;
+/*
+ * Exception 종류
+ * 1. Checked Exception
+ * 	반드시 try ~ catch 또는 throws 예약어로 처리해야한다.
+ * 	예외처리하지 않을 시 컴파일 에러 발생.
+ * 
+ * 2. Unchecked Exception
+ * 	예외 처리를 하지 않아도 컴파일 시 에러가 발생하지 않는다.
+ */
+public class ExceptionClass04 {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i< 10; i++) {
+			System.out.println(i);
+			
+			try {
+				Thread.sleep(1000); // 1초 일시정지
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
