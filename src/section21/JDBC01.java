7277bd96725028f75dd9a3883e1d81abfebcb9a1
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-06-29 21:49:53 +0900

    javaSE JDBC

---
+++ src/section21/JDBC01.java
@@ -0,0 +1,13 @@
+package section21;
+/*
+ * JDBC(JAVA DataBase Connectivity)
+ * 	자바 어플리케이션에서 데이터베이스와 통신할 수 있는 API
+ * 
+ * JDBC 설정
+ * 	해당 DBMS 관련 jdbc.jar 추가해야한다.
+ * 	ex) 오라클 - ojdbc8.jar
+ * 		[Build path] -> [Configure Build path ...] -> [Libraries] 탭 -> [Add External JARs ...] 로 ojdbc8.jar 파일 추가
+ */
+public class JDBC01 {
+
+}
